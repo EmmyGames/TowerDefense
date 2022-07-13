aac2c3a9c2bed57c4a0441f36b2dfd3cfe051454
Erick Latshaw <erickjlatshaw.com>
2022-07-13 15:48:34 -0400

    Add ability to tell if the placement is valid

--- TowerDefense/Scripts/validityCheck.gd
+++ TowerDefense/Scripts/validityCheck.gd
@@ -1,10 +1,17 @@
 extends RigidBody
 
+export (Array, Resource) var things
 var isValid: bool
 
 func _ready():
 	connect("body_entered", self, "collision_detection")
 	
 
+# Make sure the group goes on the rigidbody of things that have rbs.
 func collision_detection(body):
-	print("Col")
+	if body.is_in_group("not_valid"):
+		print("no")
+		# TODO: change mats
+		# It's probably going to be easier to store all the things that need different mats in a list manually.
+	else:
+		print("yes")
